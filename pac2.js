84b7987f0f8336429aed5afa4834c0316e1da602
rodriglz <rodriglz@uoc.edu>
2023-01-21 22:59:14 +0100

    first version

--- pac2.js
+++ pac2.js
@@ -1,1 +1,119 @@
-alert('que pasa tio?')
+var [num1, num2, output] = [ask_number('num1'), ask_number('num2'), ''] // ES6
+var msg = `Números: ${num1} i ${num2}`
+write_line(msg)
+output += msg + "\n"
+
+// Sumar tots dos números. 
+msg = "La suma dels dos números és: " + (+num1 + +num2)
+write_line(msg)
+output += msg + "\n"
+
+num1 = parseInt(num1)
+num2 = parseInt(num2)
+
+// Mostrar si tots dos números introduïts són primers, si no ho són mostrar a l'usuari que no són primers tots dos o un. 
+if (es_primer(num1) && es_primer(num2)){
+    msg = "Els dos números són primers"
+    write_line(msg)
+    output += msg + "\n"
+}
+else {
+    if (es_primer(num1)) {
+        msg = "El número " + num1 + " és primer"
+        write_line(msg)
+        output += msg + "\n"
+    }
+    else if (es_primer(num2)) {
+        msg = "El número " + num2 + " és primer"
+        write_line(msg)
+        output += msg + "\n"
+    }
+    else {
+        msg = "Cap número és primer"
+        write_line(msg)
+        output += msg + "\n"
+    }
+}
+
+// Mostrar si tots dos números introduïts són parells, si no ho són, mostrar a l'usuari que no són parells tots dos o un. 
+if (es_parell(num1) && es_parell(num2)){
+    msg = "Els dos números són parells"
+    write_line(msg)
+    output += msg + "\n"
+}
+else {
+    if (es_parell(num1)) {
+        msg = "El número " + num1 + " és parell"
+        write_line(msg)
+        output += msg + "\n"
+    }
+    else if (es_parell(num2)) {
+        msg = "El número " + num2 + " és parell"
+        write_line(msg)
+        output += msg + "\n"
+    }
+    else {
+        msg = "Cap número és parell"
+        write_line(msg)
+        output += msg + "\n"        
+    }
+}
+
+// Si el número 1 és menor que el número 2, imprimiu només els nombres parells en el rang entre num1 i num2 en forma ascendent. 
+if (num1 < num2){
+    for (n=num1; n<=num2; n++){
+        if (es_parell(n)){
+            document.write(n + " ")
+            output += n + " "
+        }        
+    }
+}
+// Si el número 1 és més gran que el número 2, imprimiu només els nombres imparells en el rang entre num1 i num2 en forma descendent.
+else if (num1 > num2){
+    for (n=num1; n>=num2; n--){
+        if (!es_parell(n)){
+            document.write(n + " ")
+            output += n + " "
+        }
+    }
+}
+// Si el número 1 és igual al número 2, imprimiu només un número; si és primer , si és parell i com que són iguals el rang només imprimeix el mateix número.
+else {
+    write_line(num1)
+    output += num1
+}
+
+alert(output)
+
+function es_parell(num){
+    if (num%2 == 0)
+        return true
+    else
+        return false
+}
+
+
+
+function es_primer(num){
+    for (n=2; n<num; n++){
+        if (num%n == 0){
+            return false
+        }        
+    }
+    return true
+}
+
+function write_line(line){
+    document.write(line)
+    document.write('<br>')
+}
+
+function ask_number(str){
+    var valid = false
+    while (!valid){
+        n1 = prompt(`Introdueix un número ${str} entre 1 i 50:`)
+        if (parseInt(n1)>=1 && parseInt(n1)<=50)
+            valid = true
+    }
+    return n1
+}
